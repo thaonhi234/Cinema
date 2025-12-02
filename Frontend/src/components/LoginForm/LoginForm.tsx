cfc41d363648dd137b2f862b8d6e19237eb2c183
NgTGiang <ngtgiang59@gmail.com>
2025-12-02 10:08:12 +0700

    Add login logo

--- Frontend/src/components/LoginForm/LoginForm.tsx
+++ Frontend/src/components/LoginForm/LoginForm.tsx
@@ -3,13 +3,25 @@ import { MdOutlineEmail } from "react-icons/md";
 import { TbLockFilled } from "react-icons/tb";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
+import { FiFilm } from "react-icons/fi";
 
 
 export default function LoginForm() {
     return (
         <div className="login-container">
             <form action="">
-                <h1>Login</h1>
+                <div className='login-titile'>
+                    <button className='logo-icon'>
+                        <FiFilm />
+                    </button>
+                    <h1>CINEMAX</h1>
+                </div>
+
+                <div className='login-description'>
+                    <h2>Welcome Back</h2>
+                    <p>Sign in to continue your movie journey</p>
+                </div>
+
                 <div className="input-box">
                     {/* <label htmlFor="username">Username</label> */}
                     <MdOutlineEmail className="icon"/>
@@ -24,13 +36,13 @@ export default function LoginForm() {
 
                 <div className="remember-forgot">
                     <label><input type="checkbox"/>Remember me</label>
-                    <a href="#">Forgot password?</a>
+                    <a className='forgot' href="#">Forgot password?</a>
                 </div>
 
                 <button type="submit" className="login-button">Sign in</button>
 
                 <div className="register-link">
-                    <p>Don't have an account? <a href="#">Register</a></p>
+                    <p>Don't have an account? <a href="#">Sign Up</a></p>
                 </div>
                 <div className="continue-with">
                     <p>or continue with</p>
